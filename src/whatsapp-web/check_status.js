0adff00536ef9ac5521d5f2487fdfad14f2d91e1
Ahmed <ahmed.shakeel.work@gmail.com>
2021-06-01 01:10:03 +0500

    #comment: updated check status route for better results

--- src/whatsapp-web/check_status.js
+++ src/whatsapp-web/check_status.js
@@ -3,21 +3,26 @@ var webdriver = require("selenium-webdriver");
 const checkStatus = async (driver) => {
     const By = webdriver.By;
     return new Promise(async (myResolve, myReject) => {
-        try {
-            console.log("checking status .....")
-            const searchContact = await driver.findElement(By.xpath('//*[@id="side"]/div[1]/div/label/div/div[2]'));
-            if (searchContact) {
-                const output = {
-                    isActive: true
+        driver.navigate().refresh();
+        setTimeout(
+            async () => {
+                try {
+                    console.log("checking status .....")
+                    const searchContact = await driver.findElement(By.xpath('//*[@id="side"]/div[1]/div/label/div/div[2]'));
+                    if (searchContact) {
+                        const output = {
+                            isActive: true
+                        }
+                        myResolve(output);
+                    }
+                } catch (error) {
+                    const output = {
+                        isActive: false
+                    }
+                    myResolve(output);
                 }
-                myResolve(output);
             }
-        } catch (error) {
-            const output = {
-                isActive: false
-            }
-            myResolve(output);
-        }
+            , 5000);
     })
 }
 
